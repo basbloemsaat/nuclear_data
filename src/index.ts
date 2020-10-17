80943e4264cacf0b3c4f58cda9dfc0b12ba55e07
Bas Bloemsaat <bas@bloemsaat.com>
2020-10-17 13:15:49 +0200

    going forward

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import "./css/pndiagram.css"
 
 import nubase from "./modules/nubase2016";
 
-import "./modules/nubase2016";
+import "./modules/pndiagram";
---
+++ src/modules/pndiagram.ts
@@ -0,0 +1,4 @@
+import nubase2016 from "./nubase2016";
+
+let x = nubase2016;
+console.log(x);
