07cbfa427e2d0b0fd801f97a9fe95901f80fd62b
Bas Bloemsaat <bas@bloemsaat.com>
2020-10-17 23:37:18 +0200

    stable isotopes

--- src/modules/nubase2016.ts
+++ src/modules/nubase2016.ts
@@ -5656,9 +5656,18 @@ class NuBase2016 {
         }
       })
       .map((row) => {
+        let A = Number.parseInt(row.substring(0, 3), 10);
+        let Z = Number.parseInt(row.substring(4, 7), 10);
+
+        let half_life:any = row.substring(60, 69).trim();
+        if(half_life != 'stbl') {
+          half_life =  Number.parseFloat(half_life);
+        }
+
         let rv = {
-          A: Number.parseInt(row.substring(0, 3), 10), // mass number
-          Z: Number.parseInt(row.substring(4, 7), 10), // atomic number
+          A: A, // mass number
+          Z: Z, // atomic number, number of protons
+          N: A-Z, // number of neutrons
           level: row.substring(7, 9).trim(), //?
           element: row.substring(11, 17).trim(),
           mass_excess: Number.parseFloat(row.substring(18, 29).trim()), //keV
@@ -5666,7 +5675,7 @@ class NuBase2016 {
           exitation_energy: row.substring(38, 48).trim(), //keV
           exitation_energy_s: row.substring(48, 56).trim(),
           exitation_energy_origin: row.substring(56, 60).trim(),
-          half_life: Number.parseFloat(row.substring(60, 69)),
+          half_life: half_life,
           half_life_unit: row.substring(69, 71).trim(),
           half_life_s: Number.parseFloat(row.substring(72, 78)),
           Jpi: row.substring(79, 92).trim(),
--- src/modules/zndiagram.ts
+++ src/modules/zndiagram.ts
@@ -6,12 +6,22 @@ let data: Array<Object> = nubase2016.full;
 
 class ZNDiagram {
   _svg: any; // any to be able to add { passive: false } to the wheel event
-  _g: d3.Selection<SVGElement, {}, HTMLElement, any>;
+  _g_canvas: any;
+  _g_left_axis: any; // any because node().getBBox();
+  _g_right_axis: any; // any because node().getBBox();
+  _g_top_axis: any; // any because node().getBBox();
+  _g_bottom_axis: any; // any because node().getBBox();
   _zoomlevel: number;
   _width: number;
   _height: number;
+  _canvas_width: number;
+  _canvas_height: number;
   _center_x: number;
   _center_y: number;
+  _offset_x: number;
+  _offset_y: number;
+  _x: d3.ScaleBand<any>;
+  _y: d3.ScaleBand<any>;
 
   constructor(svg: d3.Selection<SVGElement, {}, HTMLElement, any>) {
     this._svg = svg;
@@ -22,48 +32,123 @@ class ZNDiagram {
   }
 
   _init_chart(): void {
-    this._zoomlevel = 0;
+    this._zoomlevel = 1;
 
-    // width and heigth
+    this._g_canvas = this._svg.append("g").classed("canvas", true);
+    this._g_canvas.append("rect").classed("background", true); // background rect as target for wheel event
+    this._g_left_axis = this._svg.append("g").classed("left", true);
+    this._g_right_axis = this._svg.append("g").classed("right", true);
+    this._g_top_axis = this._svg.append("g").classed("top", true);
+    this._g_bottom_axis = this._svg.append("g").classed("bottom", true);
+
+    let rf = function (element: string): Array<string> {
+      return Object.keys(
+        data.reduce((a: any, v: any) => {
+          a[v[element]] = true;
+          return a;
+        }, {})
+      ).sort((a: any, b: any) => {
+        return a - b;
+      });
+    };
+
+    let z_s = rf("Z");
+    let n_s = rf("N");
+
+    this._x = d3.scaleBand(z_s, [0, 500]);
+    this._y = d3.scaleBand(n_s, [500, 0]);
+
+    this._resize_chart();
+  }
+
+  _resize_chart(): void {
     this._width = this._svg.node().clientWidth;
     this._height = this._svg.node().clientHeight;
+    this._svg.attr("viewBox", `0 0 ${this._width} ${this._height}`);
 
     this._center_x = Math.floor(this._width / 2);
     this._center_y = Math.floor(this._height / 2);
 
-    this._zoom_viewBox();
+    this._draw_axes(this._width, this._height); // first time drawing axes full height and width are used. First draw is used for measurements;
 
-    // let x = reduce_function
+    let left_axis_width = Math.ceil(
+      this._g_left_axis.node().getBBox()["width"]
+    );
+    let right_axis_width = Math.ceil(
+      this._g_right_axis.node().getBBox()["width"]
+    );
 
-    let z_s = Object.keys(
-      data.reduce((a: any, v: any) => {
-        a[v.Z] = true;
-        return a;
-      }, {})
-    ).sort((a: any, b: any) => {
-      return a - b;
-    });
+    let top_axis_height = Math.ceil(
+      this._g_top_axis.node().getBBox()["height"]
+    );
+    let bottom_axis_height = Math.ceil(
+      this._g_bottom_axis.node().getBBox()["height"]
+    );
 
-    let n_s = Object.keys(
-      data.reduce((a: any, v: any) => {
-        a[v.A - v.Z] = true;
-        return a;
-      }, {})
-    ).sort((a: any, b: any) => {
-      return a - b;
-    });
+    this._canvas_width = this._width - left_axis_width - right_axis_width;
+    this._canvas_height = this._height - top_axis_height - bottom_axis_height;
 
-    console.log(n_s);
+    this._g_canvas.attr(
+      "transform",
+      `translate(${left_axis_width},${top_axis_height})`
+    );
+
+    d3.select("rect.background")
+      .attr("width", this._canvas_width)
+      .attr("height", this._canvas_height);
+
+    this._g_left_axis.attr(
+      "transform",
+      `translate(${left_axis_width},${top_axis_height})`
+    );
+
+    this._g_right_axis.attr(
+      "transform",
+      `translate(${left_axis_width + this._canvas_width},${top_axis_height})`
+    );
+
+    this._g_top_axis.attr(
+      "transform",
+      `translate(${left_axis_width},${top_axis_height})`
+    );
+
+    this._g_bottom_axis.attr(
+      "transform",
+      `translate(${left_axis_width},${top_axis_height + this._canvas_height})`
+    );
+
+    this._draw_axes(this._canvas_width, this._canvas_height); // first time drawing axes actual canvas dimensions are used.
   }
 
-  _zoom_viewBox(): void {
-    // TODO: zoom
-    this._svg.attr("viewBox", `0 0 ${this._width} ${this._height}`);
+  // draw the axes
+  _draw_axes(width: number, height: number): void {
+    this._x.range([0, width]);
+    this._y.range([height, 0]);
+
+    let x_ticks = this._x.domain().filter(function (d, i) {
+      return !(i % 10);
+    });
+    let y_ticks = this._y.domain().filter(function (d, i) {
+      return !(i % 10);
+    });
+
+    let left_axis = d3.axisLeft(this._y).tickSizeOuter(0).tickValues(y_ticks);
+    this._g_left_axis.call(left_axis);
+    let right_axis = d3.axisRight(this._y).tickSizeOuter(0).tickValues(y_ticks);
+    this._g_right_axis.call(right_axis);
+
+    let top_axis = d3.axisTop(this._x).tickSizeOuter(0).tickValues(x_ticks);
+    this._g_top_axis.call(top_axis);
+    let bottom_axis = d3
+      .axisBottom(this._x)
+      .tickSizeOuter(0)
+      .tickValues(x_ticks);
+    this._g_bottom_axis.call(bottom_axis);
   }
 
   _init_zoom(): void {
     const me = this;
-    this._svg.on(
+    this._g_canvas.on(
       "wheel",
       function (e: WheelEvent) {
         e.preventDefault();
@@ -79,28 +164,50 @@ class ZNDiagram {
   }
 
   _zoom_in(e: Event): void {
-    // console.log("zoom in");
+    this._zoomlevel++;
+    console.log("zoom in", e);
   }
 
   _zoom_out(e: Event): void {
+    this._zoomlevel = Math.max(1, this._zoomlevel - 1);
     // console.log("zoom out");
   }
 
   _init_boxes(): void {
-    this._g = this._svg.append("g").classed("boxes", true);
-    let g = this._g;
+    let g = this._g_canvas;
 
     const boxes = g.selectAll("g.boxcontainer").data(data);
 
     // add the boxes
-    boxes.enter().append("g").classed("box", true);
-    this._update_boxes(boxes.merge(boxes.enter()));
+    let newboxes = boxes.enter().append("g").classed("boxcontainer", true);
+    newboxes.append("rect").classed("box", true);
+
+    this._update_boxes();
   }
 
-  _update_boxes(boxes?: d3.Selection<BaseType, {}, SVGElement, any>): void {
-    if (boxes === undefined) {
-      boxes = this._g.selectAll("g.boxcontainer");
-    }
+  _update_boxes(): void {
+    let boxes = this._g_canvas.selectAll("g.boxcontainer");
+    boxes.attr("transform", (d: any) => {
+      let x = this._x(d["Z"]);
+      let y = this._y(d["N"]);
+      return `translate(${x},${y})`;
+    });
+
+    boxes
+      .selectAll("rect.box")
+      .attr("width", this._x.bandwidth())
+      .attr("height", this._y.bandwidth())
+      .style("fill", (d: any) => {
+        // console.log(d);
+        if(d.half_life == "stbl") {
+          return '#000'
+        }
+        return "#aaa";
+      });
+    // .attr('transform', d => {
+    //   console.log(d);
+    //   return 'translate(0,0)'
+    // })
 
     // boxes are filled depending on zoom level
   }
