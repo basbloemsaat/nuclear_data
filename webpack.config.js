c3e1415cd46c1aa18d91ee80716e74647aab0e4d
Bas Bloemsaat <bas@bloemsaat.com>
2021-01-16 19:33:46 +0100

    bla

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.ts",
+    index: "./source/index.ts",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/,
+        exclude: [/node_modules/,/pre_docker/]
       },
-
+      
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+        exclude: [/node_modules/,/pre_docker/]
       },
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
+        exclude: [/node_modules/,/pre_docker/]
       },
     ],
   },
