3e69f3ca723cc379a91d42160c8b651b67255a88
Bas Bloemsaat <bas@bloemsaat.com>
2021-01-17 19:29:38 +0100

    redo 1

---
+++ source/site.ts
@@ -0,0 +1,5 @@
+import * as d3 from "d3";
+
+console.log("xyz");
+
+d3.select("footer").text("Bas Bloemsaat 2021");
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
 module.exports = {
   mode: "development",
   entry: {
     index: "./source/index.ts",
+    chart_of_nucleides: "./source/chart_of_nucleides.ts",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: [/node_modules/,/pre_docker/]
+        exclude: [/node_modules/, /pre_docker/],
       },
-      
+
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-        exclude: [/node_modules/,/pre_docker/]
+        exclude: [/node_modules/, /pre_docker/],
       },
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
-        exclude: [/node_modules/,/pre_docker/]
+        exclude: [/node_modules/, /pre_docker/],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "source/index.ejs",
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "source/chart_of_nucleides.ejs",
+      filename: "chart_of_nucleides.html",
+      chunks: ["chart_of_nucleides"],
+    }),
+  ],
 };
