7da3602b0e0ae8e73d8979f3025133389f05eaf1
Bas Bloemsaat <bas@bloemsaat.com>
2021-01-29 21:34:52 +0100

    bla

--- source/chart_of_nucleides.ts
+++ source/chart_of_nucleides.ts
@@ -1,5 +1,5 @@
-import "./site.ts";
-import { Diagram } from "./modules/diagram.ts";
+import "./site";
+import { Diagram } from "./modules/diagram";
 import * as d3 from "d3";
 
 const diag = new Diagram(d3.select("svg#zndiagram"));
